5775f87f091b25875a3d553858bed8460251126e
nsaiyed899 <nsaiyed899@gmail.com>
2023-01-19 14:27:01 +0530

    bhai mene dasboard banaya hai

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Candidate from "./Component/Candidate";
+import Dashboard from "./Component/Dashboard";
 export default function Index() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<App />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/form" element={<App />} />
         <Route path="/candidate-listing" element={<Candidate/>} />
         <Route path="/user-management" element={<App />} />
