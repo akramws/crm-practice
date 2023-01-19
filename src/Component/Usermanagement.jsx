a991c8292d93e1e39ba51b12c6aaa00ab08f9dfc
FaizanKhanPathan <pathanfaizan.1999@gmail.com>
2023-01-19 14:35:05 +0530

    faizan

---
+++ src/Component/Usermanagement.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Style/Usermanagement.css';
+
+const Usermanagement = () => {
+  return (
+    <div>
+        <div className='userHeader'>
+                Header
+        </div>
+        <div className='usermanagementmaindiv'>
+            <div className='usermanagementmain'>
+                <header>
+                        
+                </header>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Usermanagement
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Candidate from "./Component/Candidate";
+import Usermanagement from "./Component/Usermanagement";
 export default function Index() {
   return (
     <BrowserRouter>
@@ -9,7 +10,7 @@ export default function Index() {
         <Route path="/login" element={<App />} />
         <Route path="/form" element={<App />} />
         <Route path="/candidate-listing" element={<Candidate/>} />
-        <Route path="/user-management" element={<App />} />
+        <Route path="/user-management" element={<Usermanagement />} />
       </Routes>
     </BrowserRouter>
   );
