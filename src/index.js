018c49c0a3071181cae30f4ae5bf1202d1c56afb
Vikas Kumar <117701126+CoderVikas94@users.noreply.github.com>
2023-01-19 13:33:44 +0530

    component add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Candidate from "./Component/Candidate";
 export default function Index() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<App />} />
         <Route path="/form" element={<App />} />
-        <Route path="/candidate-listing" element={<App />} />
+        <Route path="/candidate-listing" element={<Candidate/>} />
         <Route path="/user-management" element={<App />} />
       </Routes>
     </BrowserRouter>
