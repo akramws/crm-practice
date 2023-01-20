704a85e66a3fc686b172a492c92803e0e7edf53b
Sohail037 <alexjason261@gmail.com>
2023-01-20 12:58:25 +0530

    I done my work

--- src/Component/Candidateform.jsx
+++ src/Component/Candidateform.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
+import CandidateFormBody from './CandidateFormBody';
 import './Style/Form.css';
-const Candidateform = () => {
+const CandidateForm = () => {
   return (
-    <div>
-    <div className='headerdiv'>
-<button>on</button>
-<div>
-    123
-</div>
-<div>
-    <button></button>
-    <span>hasf</span> 
-    <span>hasf</span> 
-    </div>
-    </div>
-    <div>
+    <>
+      <div>
+        <div className='headerdiv'>
+          <button>on</button>
+          <div>
+            123
+          </div>
+          <div>
+            <button></button>
+            <span>hasf</span>
+            <span>hasf</span>
+          </div>
+        </div>
+        <div>
 
-    </div>
-    <div>
+        </div>
+        <div>
 
-    </div>
-    </div>
+        </div>
+      </div>
+      <CandidateFormBody />
+    </>
   )
 }
 
-export default Candidateform
+export default CandidateForm
--- src/index.js
+++ src/index.js
@@ -6,24 +6,20 @@ import Header from "./Component/Header";
 import Candidate from "./Component/Candidate";
 import UserManagement from "./Component/UserManagement";
 import Dashboard from "./Component/Dashboard";
-import Candidateform from "./Component/Candidateform";
+import CandidateForm from "./Component/CandidateForm";
 import LoginPage from "./Component/LoginPage";
 export default function Index() {
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
-        <Route path="/login" element={<App />} />
-        <Route path="/form" element={<App />} />
         <Route path="/candidate-listing" element={<Candidate/>} />
         <Route path="/user-management" element={<UserManagement />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/form" element={<Candidate />} />
         <Route path="/candidate-listing" element={<App />} />
-        <Route path="/form" element={<Candidateform />} />
+        <Route path="/form" element={<CandidateForm />} />
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        <Route path="/form" element={<App />} />
         <Route path="/candidate-listing" element={<Candidate/>} />
       </Routes>
     </BrowserRouter>
