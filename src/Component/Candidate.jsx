082db3c58b94f0e30d8d1587a00d654c5f1d323c
Vikas Kumar <117701126+CoderVikas94@users.noreply.github.com>
2023-01-20 08:41:16 +0530

    pushing candidate component

--- src/Component/Candidate.jsx
+++ src/Component/Candidate.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Style/Candidate.css";
 import text from "../assets/images/Wiseskulls_Text.png";
+import {BiSearch} from 'react-icons/bi'
+import {BiChevronDown,BiReset} from 'react-icons/bi'
+import {TbDownload} from 'react-icons/tb'
+import {RiFilter2Fill} from 'react-icons/ri'
 
 const Candidate = () => {
   return (
@@ -10,20 +14,204 @@ const Candidate = () => {
         <nav className="candidate-navbar">
           <div className="search-navbar">
             <div className="search-input">
-              <span>search logo</span>
+              <span className="search-icon"><BiSearch style={{fontSize : "24px"}}/></span>
               <input type="text" placeholder="Search any.." />
-              <span></span>
+              <span><button className="search-btn">Search Any<BiChevronDown style={{fontSize : "21px"}}/></button></span>
             </div>
           </div>
           <div className="candidate-logo">Candidates' Listing</div>
           <div className="nav-btn">
-            <button>Reset</button>
-            <button>Export</button>
-            <button>User ManageMent</button>
+            <button className="btn-reset"><BiReset style={{fontSize : "16px"}}/>Reset</button>
+            <button className="btn-export"><TbDownload style = {{fontSize : "16px"}}/>Export</button>
+            <button className="btn-usermanage">User ManageMent</button>
           </div>
         </nav>
-        <main className="candidate-main"></main>
-        <footer className="candidate-footer"></footer>
+        <main className="candidate-main">
+          <table>
+            <thead>
+              <tr>
+                <th>ID <RiFilter2Fill/></th>
+                <th>Job Title <RiFilter2Fill/></th>
+                <th>Candidate's Name <RiFilter2Fill/></th>
+                <th>User <RiFilter2Fill/></th>
+                <th>Contacts <RiFilter2Fill/></th>
+                <th>Profile Source <RiFilter2Fill/></th>
+                <th>Available In Job Market <RiFilter2Fill/></th>
+                <th>Current Location <RiFilter2Fill/></th>
+                <th>Visa Type<RiFilter2Fill/></th>
+                <th>Job Location<RiFilter2Fill/></th>
+                <th>Sales Person<RiFilter2Fill/></th>
+                <th>Hiring Manager<RiFilter2Fill/></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              <tr>
+                <td>125869</td>
+                <td>Java Developer</td>
+                <td>Vinay Sharma</td>
+                <td>Amaan</td>
+                <td>9101267898</td>
+                <td>Linkedin</td>
+                <td>Yes</td>
+                <td>New York</td>
+                <td>H1 N1</td>
+                <td>New york city</td>
+                <td>DigVijay</td>
+                <td>Parth</td>
+              </tr>
+              
+            </tbody>
+          </table>
+
+        </main>
+        <footer className="candidate-footer">
+          <div className="entries">Showing 1 to 10 of 5545 entries</div>
+          <div className="footer-pageresult">
+            <div>Result per page</div>
+            <input type='number'></input>
+            <button>doewn</button>
+          </div>
+          <div className="pagination-div">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>Arrpw</button>
+            <button>DArrow</button>
+
+          </div>
+        </footer>
       </div>
       <footer className="name-footer">
         <img src={text} alt="Wiseskulls" />
