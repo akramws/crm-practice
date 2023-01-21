41315f3c60f842bbd77df2c1852d3e69d2396836
nehadesai1392000 <nehadesai1392000@gmail.com>
2023-01-21 11:17:08 +0530

    NEHA
    DCSEFRT

--- src/Component/RightSidebar.jsx
+++ src/Component/RightSidebar.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
 import './Style/RightSidebar.css'
 import Jobdesk from '../assets/images/jobdesk.png'
+import { Button, Modal } from 'antd';
+import { useState } from 'react';
+
 const RightSidebar = () => {
+  const [open, setOpen] = useState(false);
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = (e) => {
+    console.log(e);
+    setOpen(false);
+  };
+  const handleCancel = (e) => {
+    console.log(e);
+    setOpen(false);
+  };
+
   return (
+<>
+
+      <Modal
+        className='right-modal'
+        open={open}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okButtonProps={{
+          disabled: true,
+        }}
+        cancelButtonProps={{
+          disabled: true,
+        }}
+      >
+        <div className='main-box-boarder' >
+        <div className='paragrap-title' >
+         <p>Job Title : Informatica MDM Developer
+         <br /> Location : Remote - Should work in CST 
+         <br /> Duration : 6+ Months (Possibility Of Extension) 
+         <br /> Implementation Partner : Infosys
+         <br /> End Client : To Be Disclosed <br />
+         <br /> JD <br />
+        <br />: Should have solid 3-4 years of experience. <br />
+        <br /> Hands on Informatica MDM experience. <br />
+        <br /> Knowledge of JAVA and Provisioning UI. <br /> 
+        <br /> SKILLS: <br />
+        <br /> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+        </div>
+        </div>
+      </Modal>
+
     <div className='right-sidebar'>
-        <img src={Jobdesk} alt="" />
+        <img src={Jobdesk} alt="" onClick={showModal} />
     </div>
+    </>
   )
 }
 
