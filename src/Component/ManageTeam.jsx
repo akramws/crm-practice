fb0447ca3ffa3843fdb47c558dd64122ec6613cb
Rohit Sindhi <jasonroy7435@gmail.com>
2023-01-23 12:27:52 +0530

    ok

--- src/Component/ManageTeam.jsx
+++ src/Component/ManageTeam.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Style/ManageTeam.css';
 import { RxCross2 } from "react-icons/rx";
+import { Popover } from 'antd';
+import UnderbarPopup from './UnderbarPopup';
 
 const ManageTeam = () => {
   return (
@@ -10,7 +12,11 @@ const ManageTeam = () => {
             <div className='manage-team-parant-div'> 
                 <div className='manageteam-head'>Manage Teams</div>
                 <div className='head-but-div'>
+
+              <Popover placement='bottomRight' content={<UnderbarPopup/>} trigger='click'>  
                 <button className='head-btn'>Creat Team</button>
+                </Popover>
+
                 <span className='head-btn-X'><RxCross2/></span>
                 </div>
             </div>
