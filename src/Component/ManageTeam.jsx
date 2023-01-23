50f98b4348e3d2436803f5b40e0eaed74178a4aa
Sohail037 <alexjason261@gmail.com>
2023-01-23 16:01:50 +0530

    okkk

--- src/Component/ManageTeam.jsx
+++ src/Component/ManageTeam.jsx
@@ -7,8 +7,7 @@ import UnderbarPopup from './UnderbarPopup';
 const ManageTeam = () => {
   return (
     
-    <div className='manageteam-main-div'>
-        <div className='page-margin-div'>
+    
             <div className='manage-team-parant-div'> 
                 <div className='manageteam-head'>Manage Teams</div>
                 <div className='head-but-div'>
@@ -19,12 +18,13 @@ const ManageTeam = () => {
 
                 <span className='head-btn-X'><RxCross2/></span>
                 </div>
-            </div>
-            <div className='manage-team-body-div'>
+            
+              <div className='manage-team-body-div'>
            
+              </div>
             </div>
-        </div>
-    </div>
+        
+    
   )
 }
 
