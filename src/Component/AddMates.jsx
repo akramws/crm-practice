6536912597e5721536caee6aa4ea8e0d38c865f8
Rohit Sindhi <jasonroy7435@gmail.com>
2023-01-24 17:00:43 +0530

    okokokokokokok

--- src/Component/AddMates.jsx
+++ src/Component/AddMates.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import { BsFillPersonFill } from 'react-icons/bs';
+import { RxCross2 } from 'react-icons/rx';
 import './Style/AddMates.css';
 
 
 const AddMates = () => {
-  return (
-    <div>
-        <div className='add-mates-page'>
-            <p className='icons-name-radio-add'><span><BsFillPersonFill/>rohit</span> <input type="radio" /></p>
-            <p></p>
-            <p></p>
+    return (
+        <div className='main-add-mates'>
+            <div className='main-add-mates-div'>
+                <div className='add-mates-div'>Add Mates</div>
+                <span className='cancel-add-mates'><RxCross2 /></span>
+            </div>
+            <div className='add-mates-page'>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+                <p className='icons-name-radio-add'><span className='spase-icone-name'><BsFillPersonFill />rohit</span> <input className='add-mates-type-inpradio' name='one' type="checkbox" /></p>
+
+            </div>
+            
         </div>
-    </div>
-  )
+    )
 }
 
 export default AddMates
